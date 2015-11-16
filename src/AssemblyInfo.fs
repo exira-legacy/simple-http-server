dd9ea15736af7ddcfe1ed3fa6df26f1043b07287
Team City <info@exira.com>
2015-11-16 18:36:09 +0000

    Bump version to 1.0.2

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("simple-http-server")>]
 [<assembly: AssemblyProductAttribute("Exira.SimpleHttpServer")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.SimpleHttpServer is a simple HTTP server to serve static files")>]
-[<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","0c4445b9ff46e2c033fde9c280b3ad8d14ddcf16")>]
+[<assembly: AssemblyVersionAttribute("1.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
+[<assembly: AssemblyMetadataAttribute("githash","bce4592bb5243c3c0db13caf77144b0305a22967")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0"
+    let [<Literal>] Version = "1.0.2"
