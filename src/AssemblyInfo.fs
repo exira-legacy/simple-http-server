6ed4285069b81552ada07b67de1b60161fabc1d8
David Cumps <david@cumps.be>
2015-12-29 22:41:31 +0100

    Target 4.6.1

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("simple-http-server")>]
 [<assembly: AssemblyProductAttribute("Exira.SimpleHttpServer")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.SimpleHttpServer is a simple HTTP server to serve static files")>]
-[<assembly: AssemblyVersionAttribute("1.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.5")>]
-[<assembly: AssemblyMetadataAttribute("githash","226d1dde7a52512f7507dcf6636fcee70e43d9cf")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","1e5734cfda4782cb6e532d54e754f9f9fca5b97f")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.5"
+    let [<Literal>] Version = "1.0.0"
